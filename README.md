07cae32b50b88880378e9d35fe545751deb08070
Vivek Lingayat <vvkvivekv@gmail.com>
2018-02-25 12:08:29 +0530

    init commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Version Control with Git
+
+This is the repo for [Udacity's Logs Analysis Project](). In the project, we have created software where analyzer can run command instead typing query.
+
+This repo contains the source code of a Logs Analysis project.
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [Creator](#creators)
+
+## Instructions
+
+* ### Installation
+ >* clone the project
+ >* open terminal in project Dir.
+ >* go in news Dir. using ```cd news``` in terminal
+ >* run ```python newsdb.py``` or ```python3 newsdb.py``` file
+
+* #### Usage
+> * type numeric values and hit enter
+>  1. What are the most popular three articles of all time?
+>  2. Who are the most popular article authors of all time?
+>  3. On which days did more than 1% of requests lead to errors?
+>  4. exit
+
+## Creators
+
+* Vivek Lingayat (Vvk)
+    - [https://github.com/vvkvivekl](https://github.com/vvkvivekl)
+    - [https://fb.com/vvksl](https://fb.com/vvksl)
+
+Required software:
+
+* Python 3
---
+++ news/newsdb.py
@@ -0,0 +1,113 @@
+# "Database code" for the DB Forum.
+
+import os
+import sys
+import psycopg2
+DBNAME = "news"
+
+
+def popular_articles():
+    conn = psycopg2.connect(database=DBNAME)
+    cursor = conn.cursor()
+    cursor.execute(
+        "select articles.title, count(log.ip) as views from log "
+        "join articles on log.path = CONCAT('/article/',articles.slug) "
+        "group by articles.title order by views desc limit 3;"
+        )
+    results = cursor.fetchall()
+    conn.close()
+    clear()
+    print "\n\nThe most popular three articles of all time's are:-"
+    i = 1
+    for result in results:
+        print "\n", i, ") \"", result[0], "\" -- ", result[1], " views"
+        i += 1
+
+
+def popular_articles_author():
+    conn = psycopg2.connect(database=DBNAME)
+    cursor = conn.cursor()
+    cursor.execute(
+        "select authors.name, count(log.ip) as views from log, "
+        "authors, articles where articles.author = authors.id and "
+        "CONCAT('/article/',articles.slug) = log.path "
+        "group by authors.name order by views desc;"
+        )
+    results = cursor.fetchall()
+    conn.close()
+    clear()
+    print "\n\nThe most popular article authors of all time's are:-"
+    i = 1
+    for result in results:
+        print "\n", i, ") \"", result[0], "\" -- ", result[1], " views"
+        i += 1
+
+
+def log_error():
+    conn = psycopg2.connect(database=DBNAME)
+    cursor = conn.cursor()
+    cursor.execute(
+        "select t_error.date, "
+        "round(((t_totals.tl - t_error.success)*"
+        "100.0/ t_totals.tl), 2) as percent "
+        "from log, (select (log.time::timestamp::date) as date, "
+        "count(log.status) as success from log where "
+        "status = '200 OK' group by date) as t_error join "
+        "(select (log.time::timestamp::date) as date2, "
+        "count(log.status) as tl "
+        "from log group by date2) as t_totals on "
+        "t_error.date = t_totals.date2 "
+        "where ((t_totals.tl - t_error.success)*100.0/t_totals.tl) > '1' "
+        "group by date, t_error.success,t_totals.tl;"
+        )
+    results = cursor.fetchall()
+    conn.close()
+    clear()
+    print "\n\nOn this day(s) more than 1%' of requests lead to errors:-"
+    i = 1
+    for result in results:
+        print "\n", i, ") \"", result[0], "\" -- ", result[1], "%"
+        i += 1
+
+
+a = 0
+
+
+def questions():
+    print "\n\nGive numric value to select query"
+    print "1) Most popular three articles of all time"
+    print "2) Most popular article authors of all time"
+    print "3) On which days did more than 1%' of requests lead to errors?"
+    print "9) To exit"
+    a = input("Ehich query you want to run: ")
+    clear()
+    print "\n\n\nLoading..."
+    display(a)
+
+
+def display(type):
+    if (type == 1):
+        popular_articles()
+        print "\nAwosome, try for more"
+        questions()
+    elif (type == 2):
+        popular_articles_author()
+        print "\nAwosome, more!!"
+        questions()
+    elif (type == 3):
+        log_error()
+        print "\nAwosome, more!!"
+        questions()
+    elif (type == 9):
+        clear()
+        sys.exit()
+    else:
+        questions()
+
+
+def clear():
+    os.system('clear')
+
+
+clear()
+display(a)
